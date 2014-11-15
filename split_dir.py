5af92de3e1f7375c8a9439dbe99bd180f3db808b
Artem Yushkov <artem.yushkov@gmail.com>
2014-11-15 17:47:29 +0200

    v0.0.1

---
+++ split_dir.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+__author__ = 'vOROn'
+
+import os
+import shutil
+import argparse
+
+def split_dir(in_path, out_path, size_tom):
+    tom = 1
+    current_size = 0
+    root_len = len(in_path.split(os.sep))
+    for (root, dirs, files) in os.walk(in_path):
+        path = root.split(os.sep)
+        for file in files:
+            current_size += os.stat(root+os.sep+file).st_size
+
+            if current_size > size_tom:
+                current_size = 0
+                tom += 1
+
+            current_dir = os.sep.join(path[root_len+1:])
+
+            if current_dir:
+                print(out_path + os.sep + str(tom) + os.sep + current_dir + os.sep + file)
+            else:
+                print(out_path + os.sep + str(tom) + os.sep + file)
+
+            new_dir = out_path + os.sep + str(tom) + os.sep + current_dir
+
+            if not os.path.exists(new_dir):
+                try:
+                    os.makedirs(new_dir)
+                except:
+                    print("Error create directory: " + new_dir)
+
+            try:
+                shutil.copyfile(root + os.sep + file, new_dir + os.sep + file)
+            except:
+                print("Error copy file: " + new_dir + os.sep + file)
+
+
+def argParser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--in_path')
+    parser.add_argument('--out_path')
+    parser.add_argument('--size', type=int)
+    return parser
+
+if __name__ == "__main__":
+    parser = argParser()
+    space = parser.parse_args()
+    if not space.in_path or not space.out_path or not space.size:
+        parser.print_help()
+    else:
+        split_dir(space.in_path, space.out_path, space.size)
